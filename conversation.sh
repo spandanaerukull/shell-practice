1069f6a0b273d24c421a15d428f3bb767f56dba6
spandana <spandu@devops.com>
2025-05-27 12:31:10 -0500

    hello-world started

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "lillu: hey lillu,how are you?"
+echo "spandu: hello lillu, i am fine. how are you doing"
+echo "lillu: i am fine too. what's up?"
+echo "spandu:nothing ,just going to warangal now, wil you come"
+echo "lillu: sorry, you carry on !i will go to kodapgal"
