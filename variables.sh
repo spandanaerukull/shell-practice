e0425b5efbae0d0c2d4802d8a1823a73bdd5d858
spandana <spandu@devops.com>
2025-05-27 12:59:55 -0500

    hello-world started

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=LILLU
+PERSON2=SPANDU
+echo "$PERSON1: hey $PERSON1,how are you?"
+echo "$PERSON2: hello $PERSON1, i am fine. how are you doing"
+echo "$PERSON1: i am fine too. what's up?"
+echo "$PERSON2:nothing ,just going to warangal now, wil you come"  
