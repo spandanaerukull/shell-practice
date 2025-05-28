332de3cc3814c03c0560fb88ff2f376ee7840ca8
spandana <spandu@devops.com>
2025-05-28 00:38:55 -0500

    hello-world started

--- special-var.sh
+++ special-var.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 $
+echo "PID of the last command in background : $!"
