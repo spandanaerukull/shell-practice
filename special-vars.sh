e07dcdf79ee19006b611461bf5dce7de5aa8902b
spandana <spandu@devops.com>
2025-05-29 18:57:57 -0500

    special-vars.sh

--- special-vars.sh
+++ special-vars.sh
@@ -2,3 +2,11 @@
 
 echo "The name of the script is: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "current user: $USER"
+echo "current shell: $SHELL"
+echo "current date: $(date)"
+echo "home directoryof user: $HOME"
+echo " current process id: $$"
+echo "PIDof the last command: $!"
