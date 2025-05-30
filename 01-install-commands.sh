c19e66109f4aa06c22efc4a6f2ab597afcf7f972
spandana <spandu@devops.com>
2025-05-30 03:24:35 -0500

    01-install-commands.sh

--- 01-install-commands.sh
+++ 01-install-commands.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: please run this script with root access"
+    exit 1 # give other than 0  up to 127
 else  
     echo "You are running this script with root access"
   
