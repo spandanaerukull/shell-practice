ad2fca6c51a64c915c8d895905900701be815e3c
spandana <spandu@devops.com>
2025-05-31 18:38:13 -0500

    colors.sh

--- colors.sh
+++ colors.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
-echo -e "\e[32m spandu linnu \e[0m "
+USERID=$(id -u)
+R=\e[31m"
+G=\e[32m"
+B=\e[34m"
+Y=\e[33m"
+N=\e[0m"
 
-echo "lillu no clorrs"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+else  
+    echo -e "You are running this script with root access"
+  
+
+fi    
+
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "MY SQL is not installed ..going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "installing MYsql is ...success"
+    else
+        echo "installing mysql is ..FA"
+        exit 1
+    fi
+
+else    
+    echo "MY SQL is already installed .. nothing to do"
+
+    
+fi
