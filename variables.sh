efe6fe1a0663d5444fcd149d0a0bae4552fbff13
Pragney Deme <spandana>
2025-09-16 15:40:26 -0500

    hello world started

--- variables.sh
+++ variables.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-PERSON1=junnu
+PERSON1=junnu 
 PERSON2=bangarm
-echo "$PERSON1: hey $PERSON2,how are you?"
+echo "$PERSON1: hey $PERSON2,how are you?" 
 echo "$PERSON2: hello $PERSON1, i am fine. how are you doing"
 echo "$PERSON1: i am fine too. what's up?"
-echo "$PERSON2:nothing ,just going to warangal now, wil you come"  
+echo "$PERSON2:nothing ,just going to warangal now, wil you come?" 
 
-echo "$PERSON1: sorry, you carry on !i will go to kodapgal"
+echo "$PERSON1: sorry, you carry on !i will go to kodapgal"git status 
+
+#variables.sh
+#variables are used to store values
+#there are two types of variables
+#1. system variables    
+#2. user-defined variables
+#system variables are defined by the system and user-defined variables are defined by the user  
+#to define a variable we use the syntax VARIABLE_NAME=value
+#to access the value of a variable we use the syntax $VARIABLE_NAME 
+#there should be no space between VARIABLE_NAME, = and value
+#by convention, variable names are in uppercase letters 
+#example of user-defined variables
+#NAME=spandana  
