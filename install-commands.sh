4f28044c3ed5076c1ea6abd035c4fb4bdb1ab118
Pragney Deme <spandana>
2025-09-17 00:27:43 -0500

    hello world started

--- install-commands.sh
+++ install-commands.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
+# this script will install mysql if the user is root user
+USERID=$(id -u) # get the user id of the current user
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] # if user id is not equal to 0
 then
-    echo "ERROR:: please run this script with root access"
+    echo "ERROR:: please run this script with root access" # if not equal to 0 then print this message
 else  
-    echo "You are running this script with root access"
+    echo "You are running this script with root access" # if equal to 0 then print this message
   
 
 fi    
 
-dnf install mysql -y
+dnf install mysql -y # install mysql
